bf60304ba2e8ea4e5497159755b61bc26f4fd8c4
luisvillegasbf23 <luisville.lv@gmail.com>
2023-01-22 13:54:41 -0300

    add: decouple

--- api/main.py
+++ api/main.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 from .routes.create_qr import qr
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
+from decouple import config
 
-app = FastAPI()
+# app = FastAPI()
+
+isProduction = config("IS_PRODUCTION")
 origins = ["*://localhost:*/*"]
+
+if isProduction == True:
+    app = FastAPI(docs_url=None, redoc_url=None)
+else:
+    app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
@@ -13,6 +21,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 
 app.include_router(qr)
 
