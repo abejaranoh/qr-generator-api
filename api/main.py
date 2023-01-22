f56ae25101be33a32eaed4667a4dd24f636355ba
luisvillegasbf23 <luisville.lv@gmail.com>
2023-01-22 05:01:25 -0300

    fix: origins cors

--- api/main.py
+++ api/main.py
@@ -5,11 +5,10 @@ from mangum import Mangum
 
 app = FastAPI()
 
-origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
